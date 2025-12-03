a7212b5acea5b2164b515d29e2fc33378754269b
mostafakamaly <maktito2008@gmail.com>
2025-12-03 22:10:17 +0200

    Add files via upload

---
+++ icy fits jsx/react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+export default function App(){
+  return (
+    <div id="page" className="site">
+      <Header />
+
+      <main>
+        {/* Your main content goes here */}
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
